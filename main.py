126c60ccf43fb7ad76dc31215453d4511c0c3153
spfave <sebpfave@gmail.com>
2020-12-19 12:39:39 -0500

    Plan start

--- main.py
+++ main.py
@@ -8,16 +8,4 @@ COMPANY_NAME = "Tesla Inc"
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 # STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+# Send a separate message with the percentage change and each article's title and description to your phone number.
