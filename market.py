9404e02d831e992072acb17601174551d3232d8d
spfave <sebpfave@gmail.com>
2020-12-19 22:32:31 -0500

    stock close percent change

--- market.py
+++ market.py
@@ -7,10 +7,6 @@ load_dotenv()
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 
-# Constants
-STOCK_DELTA_THRESHOLD = 5
-
-
 # Functions
 def is_market_day(date) -> bool:
     """  """
@@ -22,15 +18,20 @@ def previous_market_day(date):
     pass
 
 
-def last_market_close_dates():
+def last_market_close_days(date):
     """  """
     pass
 
 
 # todo: function to determine percent change in stock price at close over preceding two days
-def stock_close_percent_change(stock_data):
-    """  """
-    pass
+def stock_close_percent_change(stock) -> float:
+    """ Calculates percent change of stock close price over last two market days """
+
+    close_days = last_market_close_days()
+    close_prices = stock_close_prices(stock, close_days)
+
+    close_percent_change = round((close_prices[0]/close_prices[1]-1)*100, 1)
+    return close_percent_change
 
 
 def stock_close_prices(stock, dates):
@@ -46,7 +47,7 @@ def stock_daily_data(stock):
         "function": "TIME_SERIES_DAILY",
         "symbol": stock,
         "apikey": os.getenv("API_KEY_STOCKS"),
-    }  # default outputsize: compact, datatype: json
+    }  # default - outputsize: compact, datatype: json
 
     response = requests.get(url=url_stocks, params=parameters)
     response.raise_for_status()
