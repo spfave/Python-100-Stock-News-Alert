f25ff08886d819d8130491c92d8db04e8b25f50d
spfave <sebpfave@gmail.com>
2020-12-19 21:05:44 -0500

    cleanup revisions

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
+
+# Constants
 STOCKS = ["TSLA", "MSFT"]
 COMPANY_NAMES = ["Tesla Inc", "Microsoft Corp"]
+STOCK_DELTA_THRESHOLD = 5
 
 # STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -39,15 +42,22 @@ def get_slice_stock_daily_data(stock_symbol, dates: tuple):
 
 
 # todo: function to determine percent change in stock price at close over preceding two days
+def stock_close_percent_change(stock_data):
+    """  """
+    pass
+
+
 # todo: function to execute if stock percent change exceeds threshold
 
 # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 # todo: function get x_num of news pieces for company_name
 
+
 # STEP 3: Use https://www.twilio.com
 # Send a separate message with the percentage change and each article's title and description to your phone number.
 # todo: function to send message with stock percent change and news article title & description
+
 
 # Main
 # todo: function/script to complete steps 1-3 for all stocks in STOCK list
