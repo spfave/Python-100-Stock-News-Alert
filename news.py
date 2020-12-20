a0ac481953aeae507e4c7fc563dc820ac2c66aae
spfave <sebpfave@gmail.com>
2020-12-19 21:48:20 -0500

    news file plan

--- news.py
+++ news.py
@@ -1,5 +1,13 @@
-
-
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
 # STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+
+
+# Functions
 # todo: function get x_num of news pieces for company_name
+def news_headlines(parameter_list):
+    """  """
+    pass
