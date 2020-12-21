7a80da48343f8642cd4a3f9d6a5ce3d7587300fd
spfave <sebpfave@gmail.com>
2020-12-20 20:49:10 -0500

    news headlines collection function

--- news.py
+++ news.py
@@ -8,6 +8,21 @@ load_dotenv()
 
 # Functions
 # todo: function get x_num of news pieces for company_name
-def news_headlines(parameter_list):
-    """  """
-    pass
+def news_top_headlines(company):
+    """ Retrieve daily stock data using Alpha Vantage api """
+
+    url_news = "https://newsapi.org/v2/everything"
+    parameters = {
+        "qInTitle": company,
+        "pagesize": 3,
+        "apikey": os.getenv("API_KEY_NEWS"),
+    }
+
+    response = requests.get(url=url_news, params=parameters)
+    response.raise_for_status()
+
+    news_stories = response.json()["articles"]
+    return news_stories
+
+
+print(news_top_headlines("Tesla"))
