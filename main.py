c42609c03e432d5c5b60fb1eaa3805acaf584852
spfave <sebpfave@gmail.com>
2020-12-21 19:40:58 -0500

    mapin.py cleanup

--- main.py
+++ main.py
@@ -9,19 +9,9 @@ COMPANY_NAMES = ["Tesla Inc", "Microsoft Corp"]
 STOCK_DELTA_THRESHOLD = 5
 
 # Main
-# todo: function/script to complete steps 1-3 for all stocks in STOCK list
 today = datetime.date.today()
 for stock in STOCKS:
     stock_close_delta = market.stock_close_percent_change(stock)
     if abs(stock_close_delta) >= STOCK_DELTA_THRESHOLD:
         company = COMPANY_NAMES[STOCKS.index(stock)]
         message.message_user(company, stock_close_delta)
-
-
-# date_back_start = str(today-datetime.timedelta(days=1))
-# date_back_end = str(today-datetime.timedelta(days=2))
-
-# stock_data = get_slice_stock_daily_data(
-#     "MSFT", (date_back_start, date_back_end))
-
-# print(stock_data)
